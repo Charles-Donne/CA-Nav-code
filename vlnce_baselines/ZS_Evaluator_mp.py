a5d5fa2e2482ca428f28edfc2c8924dc7200aa24
Charles-Donne <1124975000@qq.com>
2025-11-12 13:34:13 +0800

    test

--- vlnce_baselines/ZS_Evaluator_mp.py
+++ vlnce_baselines/ZS_Evaluator_mp.py
@@ -407,6 +407,7 @@ class ZeroShotVlnEvaluatorMP(BaseTrainer):
                 return llm_destination
         
 
+
         self.llm_reply = obs['llm_reply']
         self.instruction = obs['instruction']['text']  # 原始指令
         self.sub_instructions = self.llm_reply['sub-instructions']  # 子指令列表
